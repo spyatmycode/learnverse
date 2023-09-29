1a616d0cd8a7b2fad5876598e499a3e96e8c6d86
spyatmycode <akejunifemi11@gmail.com>
2023-09-29 21:41:24 +0100

    feat(SignIn): sign in complete

--- src/Pages/AuthenticationPages/Signin.js
+++ src/Pages/AuthenticationPages/Signin.js
@@ -1,14 +1,20 @@
 import React, { useState } from 'react'
 import svg from '../../assets/E-payment and digital wallet securitylogin_svg.svg'
 import arrow from '../../assets/Arrow - Left.svg'
+import arrowmobile from '../../assets/Arrow - Leftmobile.svg'
 
 const Signin = () => {
 
-  const [signInData, setSignInData] = useState({email:"", password:""})
+  const [signInData, setSignInData] = useState({ email: "", password: "" })
 
   const handleChange = (e) => {
     e.preventDefault()
-    setSignInData({...signInData, [e.target.name]: e.target.value})
+    setSignInData({ ...signInData, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
   }
 
   console.log(signInData);
@@ -25,7 +31,10 @@ const Signin = () => {
         </div>
         <div className='lg:w-3/5 h-screen w-full'>
 
-          <div className='lg:px-[30%] px-10 flex items-center flex-col gap-5 py-20'>
+          <div className='lg:px-[30%] px-10 flex items-center flex-col gap-5 py-10'>
+            <div className='absolute left-[10%] top-[10%] lg:hidden'>
+              <img src={arrowmobile} width={"30px"} alt="arrow" />
+            </div>
             <h1 className='font-bold text-4xl'>
               Welcome
             </h1>
@@ -33,13 +42,16 @@ const Signin = () => {
               Please enter your data to continue
             </p>
           </div>
+          <div className='lg:hidden px-10 flex items-center flex-col '>
+            <img src={svg} width={"200px"} alt="mobile__svg" />
+          </div>
 
-          <form className='lg:px-[30%] px-10 py-20 flex flex-col  gap-12'>
+          <form className='lg:px-[30%] px-10 py-10 flex flex-col  gap-12' onSubmit={handleSubmit}>
             <div className=' relative'>
 
               <label htmlFor="username" className='bg-white text-[#343434B2] absolute text-sm top-[-10px] left-[20px]'>Username</label>
 
-              <input placeholder='Example@gmail.com' type="email" className='w-full border-2 border-[#67949E] py-2 px-5 rounded-full ' name='email' onChange={handleChange}/>
+              <input placeholder='Example@gmail.com' type="email" className='w-full border-2 border-[#67949E] py-2 px-5 rounded-full ' name='email' onChange={handleChange} />
 
 
             </div>
@@ -53,7 +65,7 @@ const Signin = () => {
             </div>
 
             <div className='flex w-full justify-center'>
-              <button className='border-[#9E7167] border-2 rounded-lg px-16 py-2 shadow-lg font-bold text-xl text-[#9E7167]'>
+              <button className='lg:border-[#9E7167] lg:border-2 lg:rounded-lg px-20 py-2 shadow-lg font-bold text-xl lg:text-[#9E7167] rounded-full bg-gradient-to-r from-[#8498CB] via-[#8E89A4] to-[#9E7167] text-white lg:bg-none '>
                 Login
               </button>
             </div>
@@ -64,14 +76,33 @@ const Signin = () => {
               </p>
             </div>
 
+            <div className='flex w-full'>
+              <span className='flex w-full justify-between'>
+                <p>
+                  Remember me
+                </p>
 
-            <p className='text-center'>
-          By connecting your account confirm that you agree with our <span className='font-bold'>Term and Condition</span>
-          </p>
+
+                <label class="relative inline-flex items-center cursor-pointer">
+                  <input type="checkbox" value="" class="sr-only peer" />
+                  <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+
+                </label>
+
+
+
+              </span>
+
+            </div>
+
+
+            <p className='text-center text-xs'>
+              By connecting your account confirm that you agree with our <span className='font-bold'>Term and Condition</span>
+            </p>
 
           </form>
 
-         
+
 
         </div>
       </div>
