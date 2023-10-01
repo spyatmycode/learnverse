273bbd2f206c4f636528b6b324925d7785a16e4d
JamilAmasa <jamilamasa@gmail.com>
2023-10-01 23:57:37 +0100

    fix(signup): fix username already exists

--- src/Utility/isUserNameExist.js
+++ src/Utility/isUserNameExist.js
@@ -1,12 +1,20 @@
 import { collection, getDocs, query, where } from "firebase/firestore";
 import { db } from "./FirebaseConfig";
 
-
 const isUsernameExist = async (username) => {
-    const collectionRef = collection(db, 'users')
-    const q = query(collectionRef, where('username', '==', username))
-   const querySnapshot = await getDocs(q);
+  const collectionRef = collection(db, "users");
+  const q = query(collectionRef, where("username", "==", username));
+  const querySnapshot = await getDocs(q);
 
-   return querySnapshot > 0
+  const snapshotToArray = (snapshot) => {
+    let returnArr = [];
+
+    snapshot.forEach((childSnapshot)=> {
+      returnArr.push(childSnapshot.data());
+    });
+
+    return returnArr;
+  };
+  return snapshotToArray(querySnapshot).length > 0;
 };
 export default isUsernameExist;
