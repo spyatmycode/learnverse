f63d1932c500096560a555f2de7c0d7c1129e4ba
enaikele omoh kelvin <enaikeleomoh@gmail.com>
2023-10-01 22:57:11 +0100

    Ref:fixed all

--- src/Context/AppContext.js
+++ src/Context/AppContext.js
@@ -3,7 +3,7 @@ import React, { createContext, useContext, useState } from 'react';
 export const AppContext = createContext()
 
 export const AppProvider=({children})=>{
- const [course,setCourse]=useState('chemistry')
+ const [course,setCourse]=useState('')
 return (
     <AppContext.Provider value={{course,setCourse}}>
         {children}
